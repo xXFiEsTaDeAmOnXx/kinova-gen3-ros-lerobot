862d7ee2f6479acd3dfe4e7f2442e1b0d748926f
Niklas Kleiser <niklas.kleiser@web.de>
2026-04-17 18:44:07 +0200

    Made it try aactions

--- docker/start_gen3_teleop.sh
+++ docker/start_gen3_teleop.sh
@@ -23,6 +23,9 @@ sys.path.insert(0, pkg_parent)
 import lerobot_robot_ros.config   # registriert Subklassen
 from lerobot_robot_ros.robot import KinovaGen3, SO101ROS, AnninAR4, ROS2Robot
 
+import lerobot_teleoperator_devices.config_keyboard_joint
+from lerobot_teleoperator_devices.keyboard_joint import KeyboardJointTeleop
+
 from lerobot.utils.import_utils import register_third_party_plugins
 register_third_party_plugins()
 
@@ -33,8 +36,10 @@ sys.argv = [
     "--robot.type", "kinova_gen3",
     "--robot.id", "gen3",
     "--robot.ros2_interface.namespace", "",
-    "--teleop.type", "keyboard",
+    "--teleop.type", "keyboard_joint",
     "--teleop.id", "keyboard",
+    "--teleop.arm_action_keys", "['joint_1.pos', 'joint_2.pos', 'joint_3.pos', 'joint_4.pos', 'joint_5.pos', 'joint_6.pos', 'joint_7.pos']",
+    "--teleop.gripper_action_key", "gripper.pos",
     "--display_data", "false",
 ]
 
